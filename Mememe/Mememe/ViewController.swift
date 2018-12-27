e00dc6667c5509029813570d979e3c745dcbcec0
Shotaro Oyama <shotaro12oyama@gmail.com>
2018-12-28 08:52:57 +0900

    Lesson1#11@2018/12/28

--- Mememe/Mememe/ViewController.swift
+++ Mememe/Mememe/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var count = 0
+    var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // label
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y:150, width: 60, height: 60)
+        label.text = "0"
+        view.addSubview(label)
+        self.label = label
+        
+        // button
+        let button = UIButton()
+        button.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
+        button.setTitle("Click", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        view.addSubview(button)
+        
+        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControl.Event.touchUpInside)
     }
 
+    @objc func incrementCount() {
+        self.count += 1
+        self.label.text = "\(self.count)"
+    }
 
 }
 
